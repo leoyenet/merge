7a50924c1ef279eb9173d844188d76e77a0b863e
leoyenet <voglarleon@gmail.com>
2025-05-08 19:56:22 +0200

    v1

--- main.go
+++ main.go
@@ -8,45 +8,56 @@ import (
 	"github.com/charmbracelet/huh"
 )
 
+type Data struct {
+	filePath     string
+	newName      string
+	newDirectory string
+	merging      []string
+}
+
 func main() {
 	filePath := "/home/leoyenet/Pictures"
-	firstDir := ""
-	secondDir := ""
-	fileName := ""
-	DirName := ""
-	deleteItems := []int{}
-	form := huh.NewForm(
+	data := &Data{}
+	directory := huh.NewForm(
 		huh.NewGroup(
 			huh.NewFilePicker().
-				Title("Which folders to join").
+				Title("Which folder do you want to join in").
 				Description("It will have this parent").
 				CurrentDirectory(filePath).
 				ShowHidden(true).
 				ShowPermissions(false).
 				FileAllowed(false).
 				DirAllowed(true).
-				Value(&firstDir),
-			huh.NewFilePicker().
-				Title("Which folders to join").
-				CurrentDirectory(filePath).
-				ShowHidden(true).
-				ShowPermissions(false).
-				FileAllowed(false).
-				DirAllowed(true).
-				Value(&secondDir),
+				Value(&data.filePath),
+		),
+	).WithHeight(10)
+
+	if err := directory.Run(); err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
+	fmt.Println(GetOnlyFolder(data.filePath))
+	folders, err2 := GetOnlyFolder(data.filePath)
+	if err2 != nil {
+		os.Exit(2)
+	}
+	var options []huh.Option[string]
+	for _, value := range folders {
+		options = append(options, huh.NewOption[string](filepath.Base(value), value))
+	}
+
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewMultiSelect[string]().
+				Title("which folders to merge").
+				Options(huh.NewOptions(folders...)...).
+				Value(&data.merging),
 			huh.NewInput().
 				Title("What should new Name of files be").
-				Value(&fileName),
+				Value(&data.newName),
 			huh.NewInput().
 				Title("What should directory name be").
-				Value(&DirName),
-			huh.NewMultiSelect[int]().
-				Title("Do you want to delete them").
-				Options(
-					huh.NewOption("Delete First", 0),
-					huh.NewOption("Delete Second", 1),
-				).
-				Value(&deleteItems),
+				Value(&data.newDirectory),
 		),
 	).WithTheme(huh.ThemeCatppuccin())
 	err := form.Run()
@@ -54,39 +65,30 @@ func main() {
 		fmt.Println("Uh oh:", err)
 		os.Exit(1)
 	}
-	files := []string{}
-	firstDirFiles, err1 := GetOnlyFilesInFolder(firstDir)
-	secondDirFiles, err2 := GetOnlyFilesInFolder(secondDir)
-	if err1 != nil || err2 != nil {
-		fmt.Println(err1, err2)
-	}
+	allFiles := []string{}
 
-	files = append(files, firstDirFiles...)
-	files = append(files, secondDirFiles...)
-	newDir := filepath.Dir(firstDir)
-	newPath := filepath.Join(newDir, DirName)
-	err3 := os.MkdirAll(newPath, os.ModePerm)
-	if err3 != nil {
-		os.Exit(2)
-	}
-	err4 := error(nil)
-	for i, oldFileName := range files {
-
-		newFileName := filepath.Join(newPath, fmt.Sprintf("%s_%d%s", fileName, i+1, filepath.Ext(oldFileName)))
-		fmt.Printf("Old: %v\nNew: %v\n", oldFileName, newFileName)
-		err4 = os.Rename(oldFileName, newFileName)
-		if err4 != nil {
+	for _, value := range data.merging {
+		files, err2 := GetOnlyFilesInFolder(value)
+		if err2 != nil {
 			continue
 		}
-
+		allFiles = append(allFiles, files...)
 	}
-	for _, value := range deleteItems {
-		if value == 0 {
-			os.Remove(firstDir)
-		}
-		if value == 1 {
-			os.Remove(secondDir)
-		}
+	fmt.Println(allFiles)
+	fmt.Println(len(allFiles))
+	newPath := filepath.Join(data.filePath, data.newDirectory)
+	if e := os.Mkdir(newPath, 0775); e != nil {
+		fmt.Println(e)
+		os.Exit(3)
+	}
+	for i, oldFileName := range allFiles {
+		newFilePath := filepath.Join(newPath, fmt.Sprintf("%v_%d_%v", data.newName, i, filepath.Ext(oldFileName)))
+		fmt.Printf("\ni: %d\nOld: %v\nNew: %v\n", i, oldFileName, newFilePath)
+		os.Rename(oldFileName, newFilePath)
+	}
+
+	for _, value := range folders {
+		os.Remove(value)
 	}
 
 }
@@ -100,6 +102,23 @@ func GetOnlyFilesInFolder(folderPath string) ([]string, error) {
 	var filePaths []string
 	for _, entry := range entries {
 		if entry.Type().IsRegular() {
+			fullPath := filepath.Join(folderPath, entry.Name())
+			filePaths = append(filePaths, fullPath)
+		}
+	}
+
+	return filePaths, nil
+}
+
+func GetOnlyFolder(folderPath string) ([]string, error) {
+	entries, err := os.ReadDir(folderPath)
+	if err != nil {
+		return nil, fmt.Errorf("error reading directory %s: %w", folderPath, err)
+	}
+
+	var filePaths []string
+	for _, entry := range entries {
+		if entry.Type().IsDir() {
 			fullPath := filepath.Join(folderPath, entry.Name())
 			filePaths = append(filePaths, fullPath)
 		}
